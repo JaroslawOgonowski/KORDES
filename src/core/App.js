a540716e98cc1d633213ffd0653fecaa87130c57
JaroslawOgonowski <ipro.jaroslaw.ogonowski@gmail.com>
2025-07-23 06:24:46 +0200

    app

--- src/core/App.js
+++ src/core/App.js
@@ -8,6 +8,7 @@ import { Renowacja } from "../features/renowacja-felg/renowacja";
 import { SocialSection } from "../common/Socials/SocialSection";
 import { Tiles } from "../common/Top/Tiles";
 import { Malowanie } from "../features/malowanie-proszkowe/malowanie";
+import { BrowserRouter } from "react-router-dom/cjs/react-router-dom.min";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
             <Route path="/">
               <Redirect to="/strona-glowna" />
             </Route>
+            <BrowserRouter basename="/KORDES"></BrowserRouter>
           </Switch>
         </Main>
         <SocialSection />
